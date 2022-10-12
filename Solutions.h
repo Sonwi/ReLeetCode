2a9a7bc1b1f239f98b5df0629bba9e33a1addc4b
Sonwi <592331943@qq.com>
2022-10-12 15:23:08 +0800

    codetop solutions

--- Solutions.h
+++ Solutions.h
@@ -744,6 +744,20 @@ struct CodeTop {
         else return left;
     }
 
+    // 求平方根
+    int sqrt(int x) {
+        // write code here
+        if(x == 0 || x == 1) return x;
+        int left = 1, right = x;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(mid <= x/mid && (mid+1) >x/(mid+1)) return mid;
+            if(mid > x/mid) right = mid - 1;
+            else left = mid + 1;
+        }
+        return 0;
+    }
+
 };
 
 // 146 LRU 缓存
